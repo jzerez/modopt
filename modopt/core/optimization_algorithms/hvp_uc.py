adding jz tweaked code. Tracking past HVPs explicitly. Fixing overflow issue in AMS objective calculation. Running into HFTI errors for some problems

modopt/core/optimization_algorithms/hvp_uc.py
@@ -7,6 +7,7 @@ from modopt.line_search_algorithms import Minpack2LS
 from modopt.core.merit_functions.uc_merit_function import UCMerit
 from modopt.approximate_hessians import BFGSScipy
 from modopt import CSDLAlphaProblem
+from modopt.utils.general_utils import is_positive_definite
 
 from modopt.core.optimization_algorithms.update_B import AdaptiveMultiSecant3
 
@@ -238,6 +239,14 @@ class HVPUC(Optimizer):
         except ImportError:
             raise ImportError("HiGHS cannot be imported for the QP solver.  Install it with 'pip install highspy'.")
 
+        # Histogram for Bk's 
+        # success
+        # success, non PD
+        # fail
+        bk_hist = dict()
+        qn_bk_norms = []
+        ams_bk_norms = []
+
         # Assign shorter names to variables and methods
         nx = self.nx
 
@@ -364,7 +373,7 @@ class HVPUC(Optimizer):
                     
                     continue
 
-            print('Major iteration:', itr)
+            print('\nMajor iteration:', itr)
             print("=====================================")
 
             dir_deriv_0 = np.dot(g_k, p_k)
@@ -502,8 +511,9 @@ class HVPUC(Optimizer):
 
             # m is the number of HVPs.must be <= r
             # normally use 3 HVPs per step. Incorporate more HVPs at first step
-            # NOTE: may be worth tuning 
-            m = 3 if itr > 1 else min(nx-1, 10)
+            # NOTE: The next area of investigation. How to pick the number of HVPs and the rank of the update 
+
+            m = min(nx-1, 5)
 
             # Set of HVP directions (inputs)
             S = np.ones((nx, m))
@@ -514,18 +524,30 @@ class HVPUC(Optimizer):
             Y = np.ones(S.shape)
             
             # Krylov sub-space HVPs. the ith HVP is along the direction of the (i-1)th HVP
-            for i in range(S.shape[1]):
+            for i in range(m):
                 Y[:, i] = self.hvp(x_k, S[:, i]) # Hessian-vector product with the ith column of S (the step taken)
                 ngev += 1
-                if i+1 < S.shape[1]:
+                if i+1 < m:
                     S[:, i+1] = Y[:, i]
 
-            if itr <= 3:
-                B_k, _success = self.AMS3.update_B(B_k, S, Y, x_k, r=m)
-                print(f'Doing AMS3 update for iter {itr}. Success = {_success}')
-            else:
-                QN.update(S[:, 0], Y[:, 0])
+
+            B_k, _success = self.AMS3.update_B(B_k, S, Y, x_k, r=m)
+            QN.update(S[:, 0], Y[:, 0])
+            ams_bk_norms.append(np.linalg.norm(B_k))
+            qn_bk_norms.append(np.linalg.norm(QN.B_k))
+
+            if not _success:
+                print('AMS not successful. Falling back on Quasi-Newton')
+        
                 B_k = QN.B_k * 1.0
+                bk_hist['fail'] = bk_hist.get('fail', 0) + 1
+            elif not is_positive_definite(B_k):
+                print('AMS Returns non PD B_k. Falling back on Quasi-Newton')
+                B_k = QN.B_k * 1.0
+                bk_hist['non-PD'] = bk_hist.get('non-PD', 0) + 1
+            else:
+                bk_hist['success'] = bk_hist.get('success', 0) + 1
+
 
             #######################################################
 
@@ -562,10 +584,15 @@ class HVPUC(Optimizer):
             'ngev': ngev,
             'niter': itr,
             'time': self.total_time,
-            'success': tol_satisfied
+            'success': tol_satisfied,
+            'bk hist': bk_hist,
+            'ams_bk_norms': ams_bk_norms,
+            'qn_bk_norms': qn_bk_norms,
         }
 
         # Run post-processing for the Optimizer() base class
         self.run_post_processing()
-
+        print('\n=================')
+        print(bk_hist)
+        print('=================')
         return self.results

modopt/core/optimization_algorithms/update_B.py
@@ -190,18 +190,24 @@ class AdaptiveMultiSecant3():
     def __init__(self):
         self.save_last = 3
         self.p     = 1.0 # 1.0 or 2.0. Param for computing distance weighting
-        self.gamma = 1e-2
+        self.gamma = 1e-2   # Fudge factor for tuning weights. Close to 1 means distance doesn't impact weighting. Close to zero means that normal weighting applies. This term ensures a minimum influence from far away points. 
         self.all_X = None
         self.all_S = None
         self.all_Y = None
+        self.all_m = None   # Stored HVPs per step
         self.weights = None
+        self.prev_U = None
+        self.prev_B = None
 
+        # NOTE: Need to add something here to prevent/catch overflow values of U. 
         def jax_obj(U, B, S_all, Y_all, beta, weights):
-            obj = jnp.linalg.norm(U @ U.T, ord='fro')
+            # find lowest magnitude of U. 
+            pow = jnp.min(jnp.abs(U))
+            obj = jnp.linalg.norm(U/pow @ U.T/pow, ord='fro') * pow**2
             B_new = B + U @ U.T
             c_all = weights*(B_new @ S_all - Y_all)
 
-            penalized_obj = beta * obj + jnp.linalg.norm(c_all, ord='fro')
+            penalized_obj = beta * obj + jnp.sum(jnp.linalg.norm(c_all, axis=0))
             return penalized_obj
 
         _obj = jax.jit(jax_obj)
@@ -211,6 +217,8 @@ class AdaptiveMultiSecant3():
         self._grad  = lambda U, B, S_all, Y_all, beta, weights: np.asarray(_grad(U, B, S_all, Y_all, beta, weights)[0].ravel())
 
     def update_B(self, B, S, Y, x_k, r=1):
+        m = S.shape[1]
+
         # NOTE: should we handle r and m separately? Right now we treat them as equal. 
         # What is optimal relationship between the two? 
         # r_k+1 >= m_k+1 (Rank of update needs to at least be equal to number of HVPs)
@@ -220,18 +228,22 @@ class AdaptiveMultiSecant3():
             self.all_Y = Y * 1.0
             self.all_X = x_k.reshape(-1,1) * 1.
             self.weights = np.ones((r,), dtype=np.float64)
+            self.all_m = np.array([m,])
         else:
-            # This is requried for when the number of previous steps is less than self.save_last
-            save_last = min(np.int32(self.all_S.shape[1]/r), self.save_last)
+            # # This is requried for when the number of previous steps is less than self.save_last
+            # save_last = min(np.int32(self.all_S.shape[1]/r), self.save_last)
 
             # Store history for S, Y, and X
-            self.all_S = np.hstack((S, self.all_S[:,:save_last*r]))
-            self.all_Y = np.hstack((Y, self.all_Y[:,:save_last*r]))
-            self.all_X = np.hstack((x_k.reshape(-1,1), self.all_X[:,:save_last]))
+            self.all_m = np.hstack((m, self.all_m[:self.save_last]))
+            m_total = np.sum(self.all_m)
+
+            self.all_S = np.hstack((S, self.all_S))[:,:m_total]
+            self.all_Y = np.hstack((Y, self.all_Y))[:,:m_total]
+            self.all_X = np.hstack((x_k.reshape(-1,1), self.all_X[:,:self.save_last]))
 
             dX = x_k.reshape(-1,1) - self.all_X
             distance = np.linalg.norm(dX, axis=0)
-            self.weights = np.repeat(1.0 / (1.0 + distance**self.p), r)
+            self.weights = np.repeat(1.0 / (1.0 + distance**self.p), self.all_m)
             # self.weights = np.repeat(1.0 / np.exp(distance*self.p), r)
 
         # Problem with the weights. "save last" is ambiguous. It should refer to the number of 
@@ -242,14 +254,15 @@ class AdaptiveMultiSecant3():
         # Beta is how much we care about the norm of UU^T. 
         # Here we are saying that the update for B should be dominated by
         # satisfying a weighted sum of past secant conditions.
-        beta = np.min(self.weights) * 1e-1
-
-        # Turn off weights and beta. Equal weighting for each. 
-        self.weights[:] = 1.0
-        beta = 1.0
+        beta = np.min(self.weights) * 10
 
         nx = S.shape[0]
-        x0 = np.ones((r*nx,))
+        
+        # Seed optimizer with previous U matrix, if available. 
+        if self.prev_U is None:
+            x0 = np.ones((r*nx,))
+        else:
+            x0 = np.reshape(self.prev_U, (r*nx))
 
         B = 1e-6 * np.eye(nx)
 
@@ -258,7 +271,7 @@ class AdaptiveMultiSecant3():
 
         solver_options = {
             'maxiter': 1000,
-            'ftol': 1e-6,
+            'ftol': 1e-5,
             'disp': False,
         }
 
@@ -272,10 +285,21 @@ class AdaptiveMultiSecant3():
 
         U = np.reshape(results.x, (nx, r))
 
+
         if results['success']:
+            
             B_new = B + U @ U.T
+            try: 
+                _ = np.linalg.cholesky(B_new)
+            except np.linalg.LinAlgError:
+                print('B_new is not Positive Definite!')
+                print(U)
+
+            self.prev_U = U
+            self.prev_B = B_new
         else:
             B_new = B
+            print(results['message'])
 
         print(f"Success of multi-secant update: {results['success']}")
 